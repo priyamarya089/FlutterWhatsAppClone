018018fd14a270723b443c5c31d44ec10d692301
priyamarya089 <51272216+priyamarya089@users.noreply.github.com>
2020-10-14 19:35:32 +0530

    chat screen updated

--- lib/pages/ChatScreen.dart
+++ lib/pages/ChatScreen.dart
@@ -13,8 +13,33 @@ class _MyHomePageState extends State<ChatScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Chat Screen"),
-    );
+    return ListView.builder(
+      itemCount: 10,
+      itemBuilder: (context, index){
+        return Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top:5.0),
+              child: ListTile(
+                leading: CircleAvatar(
+                  child: Text("P"),
+                ),
+                title: Row(
+                  children: [
+                    Text("Priyam Arya",style: TextStyle(fontWeight: FontWeight.bold),),
+                    Spacer(),
+                    Text("6:20 pm")
+                  ],
+                ),
+                subtitle: Text("Hello"),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left : 70.0, right:10.0),
+              child: Divider(height: 5.0,color: Colors.grey),
+            )
+          ],
+        );
+    });
   }
 }
