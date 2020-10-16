7e7f0b9ac7d0d9de9b381309207b8b1de9128a76
priyamarya089 <51272216+priyamarya089@users.noreply.github.com>
2020-10-16 15:34:56 +0530

    Floating action button updated for different tabs

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -17,28 +17,20 @@ class HomePage extends StatefulWidget {
 class _MyHomePageState extends State<HomePage>  with SingleTickerProviderStateMixin{
 
   TabController _tabController;
-  IconData floatingActionButtonIcon = Icons.chat;
 
   @override
   void initState() {
     super.initState();
 
-    _tabController = TabController(vsync: this,initialIndex: 1, length: 4);
-    // setState(() {
-    //   _tabController.addListener((){
-    //     print('my index is'+ _tabController.index.toString());
-    //     switch (_tabController.index){
-    //       case 1 : {
-    //         floatingActionButtonIcon = Icons.chat;
-    //         break;
-    //       }
-    //       case 2 :{
-    //         this.floatingActionButtonIcon = Icons.camera_alt;
-    //         break;
-    //       }
-    //     }
-    //   });
-    // });
+    _tabController = TabController(vsync: this,initialIndex: 1, length: 4)..addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
   }
 
   @override
@@ -85,14 +77,35 @@ class _MyHomePageState extends State<HomePage>  with SingleTickerProviderStateMi
           StatusScreen(),
           CallsScreen(),
       ],),
-      floatingActionButton: FloatingActionButton(
-        onPressed:(){
-        } ,
-        backgroundColor: Colors.greenAccent[700],
-        child: Icon(
-          floatingActionButtonIcon,
-          color: Colors.white,),
-      ),
+      floatingActionButton: 
+        _tabController.index == 1 ?
+        FloatingActionButton(
+          onPressed:(){
+          } ,
+          backgroundColor: Colors.greenAccent[700],
+          child: Icon(
+            Icons.chat,
+            color: Colors.white,),
+        ) 
+        : _tabController.index == 2 ?
+        FloatingActionButton(
+          onPressed:(){
+          } ,
+          backgroundColor: Colors.greenAccent[700],
+          child: Icon(
+            Icons.camera_alt,
+            color: Colors.white,),
+        ) 
+        : _tabController.index == 3 ?
+        FloatingActionButton(
+          onPressed:(){
+          } ,
+          backgroundColor: Colors.greenAccent[700],
+          child: Icon(
+            Icons.add_ic_call,
+            color: Colors.white,),
+        )
+        : null
     );
   }
 }
